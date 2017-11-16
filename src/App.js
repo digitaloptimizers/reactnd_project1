c2ea7ba30f1d927c261a14c7af647696526bb01c
guy <guy@digitaloptimizers.com>
2017-11-16 14:14:44 +0100

    Adding React Router DOM

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 // import * as BooksAPI from './BooksAPI';
 import './App.css';
 import BookShelf from './BookShelf'; // Imports the look of a single book;
+import AddBooks from './AddBooks';
 
 
 /*
@@ -61,39 +62,18 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false
+    showSearchPage: false // False = Show list, True = Show Searchbar
   }
 
   render() {
 
-    const BooksCurrentlyReading = this.props.books;
     return (
       <div className="app">
         {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
 
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-
+              <AddBooks />
 
         ) : (
-
 
           <div className="list-books">
           {/* Show this if the user is on the List Page */}
@@ -106,13 +86,13 @@ class BooksApp extends React.Component {
               based on the lists used in the too. (MyShelfs).
               FEATURE (for the future): User-managed SHELFS in the array MyShelfs perhaps
             */}
+
             {MyShelfs.map((shelf) => (
             <BookShelf key={shelf.id} BookShelfTitle={shelf.title} books={ListOfBooks} />
             ))}
-            {/* End of loop */}
 
 
-            { /*
+            { /* REMOVE THIS:
             <div className="list-books-content">
               <div>
                 <div className="bookshelf">
@@ -253,9 +233,10 @@ class BooksApp extends React.Component {
             */ }
 
             <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+              <a href="#addBook" className="AddaBook" onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
             </div>
           </div>
+
         )}
       </div>
     )
